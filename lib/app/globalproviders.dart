18095396d05fe9a66d133d5a2070e30cbead6532
Nivas Muthu M G <nivasmuthu452@gmail.com>
2022-12-18 12:11:26 +0530

    ➡️ Refactor to use provider instead of globals

---
+++ lib/app/globalproviders.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lookout/app/services/typesense.service.dart';
+import 'package:supabase_flutter/supabase_flutter.dart' as supabase;
+import 'package:typesense/typesense.dart';
+
+final navigationKeyProvider = Provider((_) => GlobalKey<NavigatorState>());
+
+final supabaseClientProvider =
+    Provider((_) => supabase.Supabase.instance.client);
+
+final typesenseServiceProvider = Provider((_) => TypesenseService(
+    client: Client(Configuration(dotenv.maybeGet('TYPESENSE_API_KEY')!,
+        nodes: {
+          Node(Protocol.https, dotenv.maybeGet('TYPESENSE_URL')!, port: 443)
+        },
+        connectionTimeout: const Duration(seconds: 2)))));
--- lib/app/screen/login.screen.dart
+++ lib/app/screen/login.screen.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lookout/app/constants.dart';
-import 'package:lookout/app/widget/signinbutton.widget.dart';
-import 'package:lookout/main.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:lookout/app/globalproviders.dart';
+import 'package:lookout/app/widget/signin_button.widget.dart';
+import 'package:supabase_flutter/supabase_flutter.dart' as supabase;
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends ConsumerWidget {
   const LoginScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       body: Center(
         child: Padding(
@@ -20,12 +21,12 @@ class LoginScreen extends StatelessWidget {
               SignInButton(
                   assetPath: ImageAssets.googleLogo,
                   label: 'Sign In with Google',
-                  onPressed: _signInWithGoogle),
+                  onPressed: () => _signInWithGoogle(ref)),
               const SizedBox(height: 16),
               SignInButton(
                   assetPath: ImageAssets.githubLogo,
                   label: 'Sign In with Github',
-                  onPressed: _signInWithGithub,
+                  onPressed: () => _signInWithGithub(ref),
                   applyForLogo: true,
                   backgroundColor: Colors.black,
                   foregroundColor: Colors.white),
@@ -36,13 +37,15 @@ class LoginScreen extends StatelessWidget {
     );
   }
 
-  Future<void> _signInWithGoogle() async {
-    await supabase.auth.signInWithOAuth(Provider.google,
+  Future<void> _signInWithGoogle(WidgetRef ref) async {
+    await ref.read(supabaseClientProvider).auth.signInWithOAuth(
+        supabase.Provider.google,
         redirectTo: 'org.luxecraft.lookout://login-callback/');
   }
 
-  Future<void> _signInWithGithub() async {
-    await supabase.auth.signInWithOAuth(Provider.github,
+  Future<void> _signInWithGithub(WidgetRef ref) async {
+    await ref.read(supabaseClientProvider).auth.signInWithOAuth(
+        supabase.Provider.github,
         redirectTo: 'org.luxecraft.lookout://login-callback/');
   }
 }
