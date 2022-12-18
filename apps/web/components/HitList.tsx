1d41c0a2b8afbb158cbad5ce1da4632470bec18e
Sabesh Bharathi <sabesh@rocketlane.com>
2022-12-18 11:54:01 +0530

    Add Web App

---
+++ apps/web/components/HitList.tsx
@@ -0,0 +1,106 @@
+/* eslint-disable @next/next/no-img-element */
+import React, { useEffect, useRef, useState } from "react";
+import Masonry from "react-masonry-css";
+import { isEqual } from "lodash";
+// import Image from 'next/image';
+import { connectInfiniteHits } from "react-instantsearch-dom";
+import SourceLogo from "../lib/SourceLogo";
+
+const breakPointObj = {
+  default: 4,
+  3000: 6,
+  2000: 5,
+  1200: 3,
+  1000: 2,
+  570: 1,
+};
+
+const Hits = ({ hits, hasMore, refineNext }) => {
+  const [bkPoint, setBkPoint] = useState(breakPointObj);
+  const sentinelRef = useRef(null);
+
+  useEffect(() => {
+    if (sentinelRef.current !== null) {
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting && hasMore) {
+            refineNext();
+          }
+        });
+      });
+
+      observer.observe(sentinelRef.current);
+
+      return () => {
+        observer.disconnect();
+      };
+    }
+  }, [hasMore, refineNext]);
+
+  useEffect(() => {
+    if (hits.length < 3) {
+      setBkPoint({
+        ...breakPointObj,
+        default: 2,
+        3000: 2,
+        2000: 2,
+        1200: 2,
+      });
+    } else if (hits.length < 4) {
+      setBkPoint({
+        ...breakPointObj,
+        default: 3,
+        3000: 3,
+        2000: 3,
+      });
+    } else if (hits.length < 5) {
+      setBkPoint({
+        ...breakPointObj,
+        3000: 4,
+        2000: 4,
+      });
+    } else if (hits.length > 5 && !isEqual(bkPoint, breakPointObj)) {
+      setBkPoint(breakPointObj);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [hits]);
+  return (
+    <Masonry className="flex" breakpointCols={bkPoint}>
+      {hits.map((hit) => (
+        <div className="w-max m-4 imgCard" key={hit.id}>
+          <div className="relative shadow-xl hover:scale-105 duration-500 skew-y-3 transition-all rounded-md cursor-pointer w-auto overflow-hidden">
+            <div className="absolute flex flex-col justify-between py-4 h-full w-full duration-500 hover:opacity-100 opacity-0 hover:dark:bg-white/40 hover:bg-black/40">
+              <div className="px-4 drop-shadow-lg">
+                <a href={hit.post_url}>
+                  <SourceLogo source={hit.source} />
+                </a>
+              </div>
+              <div className="flex overflow-scroll">
+                {hit.labels.map((label) => (
+                  <div
+                    className="mx-2 whitespace-nowrap font-silk text-white bg-black/50 text-sm font-bold  rounded-md p-1"
+                    key={label.id}
+                  >
+                    {label}
+                  </div>
+                ))}
+              </div>
+            </div>
+            <img
+              src={process.env.NEXT_PUBLIC_BLOB_URL + hit.image_url}
+              alt={hit.title}
+              className="w-full max-w-[250px] rounded-lg"
+            />
+          </div>
+        </div>
+      ))}
+      <li
+        className="ais-InfiniteHits-sentinel"
+        ref={sentinelRef}
+        aria-hidden="true"
+      />
+    </Masonry>
+  );
+};
+const HitList = connectInfiniteHits(Hits);
+export default HitList;
--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,11 +1,74 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+const plugin = require("tailwindcss/plugin");
+
+const rotateY = plugin(function ({ addUtilities }) {
+  addUtilities({
+    ".rotate-y-30": {
+      transform: "rotateY(30deg)",
+    },
+    ".rotate-y-45": {
+      transform: "rotateY(45deg)",
+    },
+    ".rotate-y-60": {
+      transform: "rotateY(60deg)",
+    },
+    ".rotate-y-90": {
+      transform: "rotateY(90deg)",
+    },
+    ".rotate-y-180": {
+      transform: "rotateY(180deg)",
+    },
+  });
+});
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  safelist: [
+    "bg-sky-500",
+    "bg-teal-500",
+    "bg-green-500",
+    "bg-amber-500",
+    "bg-rose-500",
+    "bg-pink-500",
+    "bg-fuchsia-500",
+    "bg-violet-500",
+    "bg-indigo-500",
+    "bg-blue-500",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        silk: ["Silkscreen-Regular", ...defaultTheme.fontFamily.sans],
+      },
+      textShadow: {
+        sm: "0 1px 2px var(--tw-shadow-color)",
+        DEFAULT: "0 2px 4px var(--tw-shadow-color)",
+        lg: "0 8px 16px var(--tw-shadow-color)",
+      },
+      colors: {
+        background: "#020015",
+      },
+      letterSpacing: {
+        "wide-wide": "0.2em",
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [
+    rotateY,
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          "text-shadow": (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme("textShadow") }
+      );
+    }),
+  ],
+};
