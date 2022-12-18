0f4751c147a19506fa11a31f524317636d450bac
Nivas Muthu M G <nivasmuthu452@gmail.com>
2022-12-18 12:07:14 +0530

    {} Image Document Model Definition

---
+++ lib/app/model/image_document/image_document.model.freezed.dart
@@ -0,0 +1,447 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'image_document.model.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+ImageDocument _$ImageDocumentFromJson(Map<String, dynamic> json) {
+  return _ImageDocument.fromJson(json);
+}
+
+/// @nodoc
+mixin _$ImageDocument {
+  List<String> get colors => throw _privateConstructorUsedError;
+  int get height => throw _privateConstructorUsedError;
+  String get id => throw _privateConstructorUsedError;
+  @JsonKey(name: 'image_url')
+  String get imageUrl => throw _privateConstructorUsedError;
+  List<String> get labels => throw _privateConstructorUsedError;
+  bool get nfsw =>
+      throw _privateConstructorUsedError; // ignore: invalid_annotation_target
+  @JsonKey(name: 'post_url')
+  String get postUrl => throw _privateConstructorUsedError;
+  List<dynamic> get safeSearch => throw _privateConstructorUsedError;
+  String get source => throw _privateConstructorUsedError;
+  String get sub => throw _privateConstructorUsedError;
+  List<dynamic> get text => throw _privateConstructorUsedError;
+  String get title => throw _privateConstructorUsedError;
+  int get width => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $ImageDocumentCopyWith<ImageDocument> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $ImageDocumentCopyWith<$Res> {
+  factory $ImageDocumentCopyWith(
+          ImageDocument value, $Res Function(ImageDocument) then) =
+      _$ImageDocumentCopyWithImpl<$Res, ImageDocument>;
+  @useResult
+  $Res call(
+      {List<String> colors,
+      int height,
+      String id,
+      @JsonKey(name: 'image_url') String imageUrl,
+      List<String> labels,
+      bool nfsw,
+      @JsonKey(name: 'post_url') String postUrl,
+      List<dynamic> safeSearch,
+      String source,
+      String sub,
+      List<dynamic> text,
+      String title,
+      int width});
+}
+
+/// @nodoc
+class _$ImageDocumentCopyWithImpl<$Res, $Val extends ImageDocument>
+    implements $ImageDocumentCopyWith<$Res> {
+  _$ImageDocumentCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? colors = null,
+    Object? height = null,
+    Object? id = null,
+    Object? imageUrl = null,
+    Object? labels = null,
+    Object? nfsw = null,
+    Object? postUrl = null,
+    Object? safeSearch = null,
+    Object? source = null,
+    Object? sub = null,
+    Object? text = null,
+    Object? title = null,
+    Object? width = null,
+  }) {
+    return _then(_value.copyWith(
+      colors: null == colors
+          ? _value.colors
+          : colors // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      height: null == height
+          ? _value.height
+          : height // ignore: cast_nullable_to_non_nullable
+              as int,
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      imageUrl: null == imageUrl
+          ? _value.imageUrl
+          : imageUrl // ignore: cast_nullable_to_non_nullable
+              as String,
+      labels: null == labels
+          ? _value.labels
+          : labels // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      nfsw: null == nfsw
+          ? _value.nfsw
+          : nfsw // ignore: cast_nullable_to_non_nullable
+              as bool,
+      postUrl: null == postUrl
+          ? _value.postUrl
+          : postUrl // ignore: cast_nullable_to_non_nullable
+              as String,
+      safeSearch: null == safeSearch
+          ? _value.safeSearch
+          : safeSearch // ignore: cast_nullable_to_non_nullable
+              as List<dynamic>,
+      source: null == source
+          ? _value.source
+          : source // ignore: cast_nullable_to_non_nullable
+              as String,
+      sub: null == sub
+          ? _value.sub
+          : sub // ignore: cast_nullable_to_non_nullable
+              as String,
+      text: null == text
+          ? _value.text
+          : text // ignore: cast_nullable_to_non_nullable
+              as List<dynamic>,
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      width: null == width
+          ? _value.width
+          : width // ignore: cast_nullable_to_non_nullable
+              as int,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$_ImageDocumentCopyWith<$Res>
+    implements $ImageDocumentCopyWith<$Res> {
+  factory _$$_ImageDocumentCopyWith(
+          _$_ImageDocument value, $Res Function(_$_ImageDocument) then) =
+      __$$_ImageDocumentCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {List<String> colors,
+      int height,
+      String id,
+      @JsonKey(name: 'image_url') String imageUrl,
+      List<String> labels,
+      bool nfsw,
+      @JsonKey(name: 'post_url') String postUrl,
+      List<dynamic> safeSearch,
+      String source,
+      String sub,
+      List<dynamic> text,
+      String title,
+      int width});
+}
+
+/// @nodoc
+class __$$_ImageDocumentCopyWithImpl<$Res>
+    extends _$ImageDocumentCopyWithImpl<$Res, _$_ImageDocument>
+    implements _$$_ImageDocumentCopyWith<$Res> {
+  __$$_ImageDocumentCopyWithImpl(
+      _$_ImageDocument _value, $Res Function(_$_ImageDocument) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? colors = null,
+    Object? height = null,
+    Object? id = null,
+    Object? imageUrl = null,
+    Object? labels = null,
+    Object? nfsw = null,
+    Object? postUrl = null,
+    Object? safeSearch = null,
+    Object? source = null,
+    Object? sub = null,
+    Object? text = null,
+    Object? title = null,
+    Object? width = null,
+  }) {
+    return _then(_$_ImageDocument(
+      colors: null == colors
+          ? _value._colors
+          : colors // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      height: null == height
+          ? _value.height
+          : height // ignore: cast_nullable_to_non_nullable
+              as int,
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      imageUrl: null == imageUrl
+          ? _value.imageUrl
+          : imageUrl // ignore: cast_nullable_to_non_nullable
+              as String,
+      labels: null == labels
+          ? _value._labels
+          : labels // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      nfsw: null == nfsw
+          ? _value.nfsw
+          : nfsw // ignore: cast_nullable_to_non_nullable
+              as bool,
+      postUrl: null == postUrl
+          ? _value.postUrl
+          : postUrl // ignore: cast_nullable_to_non_nullable
+              as String,
+      safeSearch: null == safeSearch
+          ? _value._safeSearch
+          : safeSearch // ignore: cast_nullable_to_non_nullable
+              as List<dynamic>,
+      source: null == source
+          ? _value.source
+          : source // ignore: cast_nullable_to_non_nullable
+              as String,
+      sub: null == sub
+          ? _value.sub
+          : sub // ignore: cast_nullable_to_non_nullable
+              as String,
+      text: null == text
+          ? _value._text
+          : text // ignore: cast_nullable_to_non_nullable
+              as List<dynamic>,
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      width: null == width
+          ? _value.width
+          : width // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$_ImageDocument implements _ImageDocument {
+  _$_ImageDocument(
+      {required final List<String> colors,
+      required this.height,
+      required this.id,
+      @JsonKey(name: 'image_url') required this.imageUrl,
+      required final List<String> labels,
+      required this.nfsw,
+      @JsonKey(name: 'post_url') required this.postUrl,
+      required final List<dynamic> safeSearch,
+      required this.source,
+      required this.sub,
+      required final List<dynamic> text,
+      required this.title,
+      required this.width})
+      : _colors = colors,
+        _labels = labels,
+        _safeSearch = safeSearch,
+        _text = text;
+
+  factory _$_ImageDocument.fromJson(Map<String, dynamic> json) =>
+      _$$_ImageDocumentFromJson(json);
+
+  final List<String> _colors;
+  @override
+  List<String> get colors {
+    if (_colors is EqualUnmodifiableListView) return _colors;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_colors);
+  }
+
+  @override
+  final int height;
+  @override
+  final String id;
+  @override
+  @JsonKey(name: 'image_url')
+  final String imageUrl;
+  final List<String> _labels;
+  @override
+  List<String> get labels {
+    if (_labels is EqualUnmodifiableListView) return _labels;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_labels);
+  }
+
+  @override
+  final bool nfsw;
+// ignore: invalid_annotation_target
+  @override
+  @JsonKey(name: 'post_url')
+  final String postUrl;
+  final List<dynamic> _safeSearch;
+  @override
+  List<dynamic> get safeSearch {
+    if (_safeSearch is EqualUnmodifiableListView) return _safeSearch;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_safeSearch);
+  }
+
+  @override
+  final String source;
+  @override
+  final String sub;
+  final List<dynamic> _text;
+  @override
+  List<dynamic> get text {
+    if (_text is EqualUnmodifiableListView) return _text;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_text);
+  }
+
+  @override
+  final String title;
+  @override
+  final int width;
+
+  @override
+  String toString() {
+    return 'ImageDocument(colors: $colors, height: $height, id: $id, imageUrl: $imageUrl, labels: $labels, nfsw: $nfsw, postUrl: $postUrl, safeSearch: $safeSearch, source: $source, sub: $sub, text: $text, title: $title, width: $width)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_ImageDocument &&
+            const DeepCollectionEquality().equals(other._colors, _colors) &&
+            (identical(other.height, height) || other.height == height) &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.imageUrl, imageUrl) ||
+                other.imageUrl == imageUrl) &&
+            const DeepCollectionEquality().equals(other._labels, _labels) &&
+            (identical(other.nfsw, nfsw) || other.nfsw == nfsw) &&
+            (identical(other.postUrl, postUrl) || other.postUrl == postUrl) &&
+            const DeepCollectionEquality()
+                .equals(other._safeSearch, _safeSearch) &&
+            (identical(other.source, source) || other.source == source) &&
+            (identical(other.sub, sub) || other.sub == sub) &&
+            const DeepCollectionEquality().equals(other._text, _text) &&
+            (identical(other.title, title) || other.title == title) &&
+            (identical(other.width, width) || other.width == width));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      const DeepCollectionEquality().hash(_colors),
+      height,
+      id,
+      imageUrl,
+      const DeepCollectionEquality().hash(_labels),
+      nfsw,
+      postUrl,
+      const DeepCollectionEquality().hash(_safeSearch),
+      source,
+      sub,
+      const DeepCollectionEquality().hash(_text),
+      title,
+      width);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$_ImageDocumentCopyWith<_$_ImageDocument> get copyWith =>
+      __$$_ImageDocumentCopyWithImpl<_$_ImageDocument>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$_ImageDocumentToJson(
+      this,
+    );
+  }
+}
+
+abstract class _ImageDocument implements ImageDocument {
+  factory _ImageDocument(
+      {required final List<String> colors,
+      required final int height,
+      required final String id,
+      @JsonKey(name: 'image_url') required final String imageUrl,
+      required final List<String> labels,
+      required final bool nfsw,
+      @JsonKey(name: 'post_url') required final String postUrl,
+      required final List<dynamic> safeSearch,
+      required final String source,
+      required final String sub,
+      required final List<dynamic> text,
+      required final String title,
+      required final int width}) = _$_ImageDocument;
+
+  factory _ImageDocument.fromJson(Map<String, dynamic> json) =
+      _$_ImageDocument.fromJson;
+
+  @override
+  List<String> get colors;
+  @override
+  int get height;
+  @override
+  String get id;
+  @override
+  @JsonKey(name: 'image_url')
+  String get imageUrl;
+  @override
+  List<String> get labels;
+  @override
+  bool get nfsw;
+  @override // ignore: invalid_annotation_target
+  @JsonKey(name: 'post_url')
+  String get postUrl;
+  @override
+  List<dynamic> get safeSearch;
+  @override
+  String get source;
+  @override
+  String get sub;
+  @override
+  List<dynamic> get text;
+  @override
+  String get title;
+  @override
+  int get width;
+  @override
+  @JsonKey(ignore: true)
+  _$$_ImageDocumentCopyWith<_$_ImageDocument> get copyWith =>
+      throw _privateConstructorUsedError;
+}
