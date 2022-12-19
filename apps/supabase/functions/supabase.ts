37fa351adc16e23f6c94b694b96b86ea906ed2e9
harisarang <harithiru2018@gmail.com>
2022-12-19 07:39:26 +0000

    update: supabase config for rate limiting

--- apps/supabase/functions/supabase.ts
+++ apps/supabase/functions/supabase.ts
@@ -1,4 +1,10 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2.2.1";
 import { SUPABASE_KEY, SUPABASE_URL } from "./config.ts";
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY, {
+    realtime: {
+        params: {
+            eventsPerSecond: 100,
+        },
+    },
+});
