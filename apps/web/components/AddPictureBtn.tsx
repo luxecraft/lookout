f498d1659cee034b14cc7edf0731c6a7c7bbd4fe
AmanRK <amankhemka.ak28@gmail.com>
2022-12-19 12:47:09 +0530

    real time

--- apps/web/components/AddPictureBtn.tsx
+++ apps/web/components/AddPictureBtn.tsx
@@ -14,6 +14,55 @@ type Props = {
 const AddPictureBtn = (props: Props) => {
   const { user, updatePosts } = props;
   const [loading, setLoading] = useState(false);
+
+  const uploadPic = async (e: React.FormEvent<HTMLInputElement>) => {
+    setLoading(true);
+    //@ts-ignore
+    const file = e.target.files[0];
+
+    if (file.size > 2048000) {
+      return;
+    }
+
+    //Check if image is not an image
+    if (!file.type.startsWith("image/")) {
+      console.log("not an image");
+      return;
+    }
+
+    const fileType = file.type.split("/")[1];
+    const uuid = uuidv4();
+    const imgPath = "/images/users/" + uuid + "." + fileType;
+
+    const channel = supabase.channel(uuid);
+    const subscribe = await new Promise((resolve) => {
+      channel.subscribe((payload) => {
+        console.log(payload);
+        resolve(payload);
+      });
+    });
+
+    if (subscribe === "SUBSCRIBED") {
+      channel.on("broadcast", { event: "progress" }, (payload) => {
+        console.log(payload);
+      });
+    }
+    const { data, error } = await supabase.storage
+      .from("master")
+      .upload(imgPath, file);
+    console.log(data, error);
+
+    const res = await axios.post("/api/uploadPhoto", {
+      uuid,
+      imgPath,
+      userImageUrl: user?.user_metadata.avatar_url,
+      userId: user?.id,
+    });
+
+    updatePosts();
+    setLoading(false);
+  }
+
   return (
     <div className="relative">
       <div
@@ -22,39 +71,7 @@ const AddPictureBtn = (props: Props) => {
        border-gray-600 shadow-xl border-opacity-30 backdrop-blur-xl py-6 px-8 rounded-xl"
       >
         <input
-          onInputCapture={async (e) => {
-            setLoading(true);
-            //@ts-ignore
-            const file = e.target.files[0];
-            console.log(file);
-
-            //Check if image is not an image
-            if (!file.type.startsWith("image/")) {
-              console.log("not an image");
-              return;
-            }
-
-            const fileType = file.type.split("/")[1];
-            const uuid = uuidv4();
-            const imgPath = "/images/users/" + uuid + "." + fileType;
-
-            const { data, error } = await supabase.storage
-              .from("master")
-              .upload(imgPath, file);
-            console.log(data, error);
-
-            const res = await axios.post("/api/uploadPhoto", {
-              uuid,
-              imgPath,
-              userImageUrl: user?.user_metadata.avatar_url,
-              userId: user?.id,
-            });
-
-            console.log(res);
-
-            updatePosts();
-            setLoading(false);
-          }}
+          onInputCapture={uploadPic}
           className="hidden"
           type="file"
           id="files"
